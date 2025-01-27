220a470cdeafb54a236b0bb3a2f759c4fabecef1
ZHARFAN <zharfanwidyadana@gmail.com>
2025-01-28 00:48:32 +0700

    memperbarui tipe data Pengeluaran

--- app/src/main/java/com/example/finalprojectpam/data/model/Pengeluaran.kt
+++ app/src/main/java/com/example/finalprojectpam/data/model/Pengeluaran.kt
@@ -3,10 +3,10 @@ package com.example.finalprojectpam.data.model
 import java.util.Date
 
 data class Pengeluaran(
-    val idPengeluaran: Int,    // ID unik untuk pengeluaran
-    val idAset: Int,           // ID aset terkait
-    val idKategori: Int,       // ID kategori terkait
-    val tanggalTransaksi: Date, // Tanggal transaksi pengeluaran
-    val total: Double,         // Jumlah pengeluaran
-    val catatan: String        // Catatan tambahan
+    val idPengeluaran: String = "",
+    val idAset: String = "",
+    val idKategori: String = "",
+    val tanggalTransaksi: Date = Date(),
+    val total: Double = 0.0,
+    val catatan: String = ""
 )
