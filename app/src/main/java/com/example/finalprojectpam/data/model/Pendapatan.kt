bb645c30b7be3a2417954fec5ae64d6a1fa88b5d
ZHARFAN <zharfanwidyadana@gmail.com>
2025-01-28 00:40:05 +0700

    membuat UI AddPendapatan dan memperbaiki tipe data Pendapatan.kt

--- app/src/main/java/com/example/finalprojectpam/data/model/Pendapatan.kt
+++ app/src/main/java/com/example/finalprojectpam/data/model/Pendapatan.kt
@@ -1,12 +1,12 @@
 package com.example.finalprojectpam.data.model
 
-import java.sql.Date
+import java.util.Date
 
 data class Pendapatan(
-    val idPendapatan: Int,     // ID unik untuk pendapatan
-    val idAset: Int,           // ID aset terkait
-    val idKategori: Int,       // ID kategori terkait
-    val tanggalTransaksi: Date, // Tanggal transaksi pendapatan
-    val total: Double,         // Jumlah pendapatan
-    val catatan: String        // Catatan tambahan
+    val idPendapatan: String = "",
+    val idAset: String = "",
+    val idKategori: String = "",
+    val tanggalTransaksi: Date = Date(),
+    val total: Double = 0.0,
+    val catatan: String = ""
 )
