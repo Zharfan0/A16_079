a7e7eeb3b280546f86f0ac357e2a5b5aac8fd973
ZHARFAN <zharfanwidyadana@gmail.com>
2025-01-28 11:50:39 +0700

    bug fix lagi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,15 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.appcompat:appcompat:1.7.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation ("androidx.fragment:fragment-ktx:1.6.1")
+    implementation ("androidx.fragment:fragment-ktx:1.8.5")
     implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation (platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation ("com.google.android.gms:play-services-base:18.2.0")
+
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
